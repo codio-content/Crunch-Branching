5d5a1a897ca0c464ab9bfb45a58f35d9e8c4c2c5
Elliott Mestas <elliott.mestas@gmail.com>
2015-06-29 18:06:59 -0400

    refactor to allow for multiple random tests

--- .guides/tests/guide-script.js
+++ .guides/tests/guide-script.js
@@ -1,26 +1,30 @@
 function t_branch1(data) {
-  var inp = TESTS.GetRandomIntegerArray(1, 20);
-  TESTS.SimpleOutputTest(data, inp, [], [inp[0] > 20 ? 1 : 0]);
+  TESTS.SimpleOutputTest(data, {min: 0, max: 20}, function(inp, vars) {
+    return [inp[0] > 20 ? 1 : 0];
+  });
 }
 
 function t_branch2(data) {
-  var inp = TESTS.GetRandomIntegerArray(2, 20);
-  TESTS.SimpleOutputTest(data, inp, [], [inp[0] + inp[1] > 20 ? 1 : 0]);
+  TESTS.SimpleOutputTest(data, [{min: 0, max: 20}, {min: 0, max: 20}], function(inp, vars) {
+    return [inp[0] + inp[1] > 20 ? 1 : 0];
+  });
 }
 
 function t_branch3(data) {
-  var inp = TESTS.GetRandomIntegerArray(1, 40)[0];
-  var expected;
-  if (inp <= 10) {
-    expected = 0;
-  } else if (inp <= 20) {
-    expected = 1;
-  } else if (inp <= 30) {
-    expected = 2;
-  } else {
-    expected = 99;
-  }
-  TESTS.SimpleOutputTest(data, [], [{name: 'Number', value: inp}], [expected]);
+  TESTS.SimpleOutputTest(data, [{name: 'Number', min: 0, max: 40}], function(inp, vars) {
+    var expected;
+    var var0 = vars[0].value;
+    if (var0 <= 10) {
+      expected = 0;
+    } else if (var0 <= 20) {
+      expected = 1;
+    } else if (var0 <= 30) {
+      expected = 2;
+    } else {
+      expected = 99;
+    }
+    return [expected];
+  });
 }
 
 TESTS.SetupButtonTest();
